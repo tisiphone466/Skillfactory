539a20a4d67d9d80bca100039c96bf814a4c4fa7
tisiphone466 <tisiphone466@gmail.com>
2021-07-26 20:34:52 +0300

    refactor: rename variables, add comments

--- module_0/main.py
+++ module_0/main.py
@@ -1,29 +1,27 @@
 
 
-def guessing_game(number):
-    print(f"Загадано число от 1 до 100")
-    try_count = 1  # счетчик попыток
-
-    step = 50  # шаг изменения предполагаемого числа
-    predict = step  # предполагаемое число
+def guess_number(number):
+    """Guess the number from 1 to 100"""
+    tries_count = 1
+    step = 50  # Step of changing predictable number
+    predict = step  # Predictable number
 
     while predict != number:
-        try_count += 1  # плюсуем попытку
+        tries_count += 1
         step = round(step/2) if step > 1 else step
         predict = predict + step if number > predict else predict - step
 
-        print('predict:', predict, 'step:', step)
-
-    print(f"Вы угадали число {number} за {try_count} попыток.")
-    return try_count
+    print(f"The number {number} was guessed in {tries_count} tries.")
+    return tries_count
 
 
-def score_game(guessing_game_func):
+def guess_benchmark(guess_number_fn):
+    """Guessing number function benchmark"""
     tries = list()
     for number in range(1, 101):
-        tries.append(guessing_game_func(number))
+        tries.append(guess_number_fn(number))
 
-    print("В среднем", sum(tries) / len(tries), "попыток")
+    print("On average", sum(tries) / len(tries), "tries")
 
 
-score_game(guessing_game)
+guess_benchmark(guess_number)
